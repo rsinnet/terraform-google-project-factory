b47f05c60bf803071f7891c7b2febf46d071f438
Ryan Sinnet <rsinnet@misorobotics.com>
2021-10-09 07:15:22 -0700

    Explain error when passing compute.googleapis.com to activate_api_identities
    Update the description of `active_api_identities` with a note to avoid
    passing compute.googleapis.com as an argument. It seems that this API
    call is not permitted despite lack of documentation. This can be seen by
    calling:
    
    ```sh
    gcloud alpha services identity create --service=compute.googleapis.com --log-http
    ```
    
    Calling this same command with, e.g., cloudbuild.googleapis.com does not
    result in an error.

--- variables.tf
+++ variables.tf
@@ -105,6 +105,7 @@ variable "activate_api_identities" {
   description = <<EOF
     The list of service identities (Google Managed service account for the API) to force-create for the project (e.g. in order to grant additional roles).
     APIs in this list will automatically be appended to `activate_apis`.
+    Do not attempt to pass `compute.googleapis.com` as it is not supported by this argument.
     Not including the API in this list will follow the default behaviour for identity creation (which is usually when the first resource using the API is created).
     Any roles (e.g. service agent role) must be explicitly listed. See https://cloud.google.com/iam/docs/understanding-roles#service-agent-roles-roles for a list of related roles.
   EOF
